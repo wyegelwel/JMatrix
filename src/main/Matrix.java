07e08bec84408126dbe4f6b2f7b5fec0f57c1159
Wil Yegelwel <wyegelwel@gmail.com>
2013-10-02 23:42:26 -0400

    Implemented matrix multiplication and created the Vector class to aid in that

--- src/main/Matrix.java
+++ src/main/Matrix.java
@@ -1,9 +1,12 @@
 package main;
 
+//TODO correct LowerTriangularMatrix and UpperTriangularMatrix so that they don't need to be square
 public class Matrix {
 	protected int _rows;
 	protected int _cols;
 	protected double[][] _m;
+	
+	public static double EQUALITY_EPSILON = 1e-10;
 	
 	public Matrix(int rows, int cols){
 		_rows = rows;
@@ -26,6 +29,14 @@ public class Matrix {
 		return _m[row][col];
 	}
 	
+	public Vector getRow(int row){
+		return Vector.rowVector(this, row);
+	}
+	
+	public Vector getColumn(int col){
+		return Vector.columnVector(this, col);
+	}
+	
 	public int rows(){
 		return _rows;
 	}
@@ -34,8 +45,12 @@ public class Matrix {
 		return _cols;
 	}
 	
-	//TODO implement. Consider making a column and row vector class and implement dot product there first
-	// Then just have a get row and get column method in matrix
+	/**
+	 * Multiplies two matrices using matrix multiplication (NOT ELEMENTWISE!)
+	 * @param left
+	 * @param right
+	 * @return
+	 */
 	public static Matrix mult(Matrix left, Matrix right){
 		if (left.cols() != right.rows()){
 			//TODO throw error
@@ -43,7 +58,11 @@ public class Matrix {
 			return null;
 		} else{
 			Matrix m = new Matrix(left.rows(), right.cols());
-			
+			for (int row = 0; row < left.rows(); row++){
+				for (int col = 0; col < right.cols(); col++){
+					m._m[row][col] = left.getRow(row).dot(right.getColumn(col));
+				}
+			}
 			return m;
 		}
 	}
@@ -68,5 +87,29 @@ public class Matrix {
 			}
 		}
 		return dest;
+	}
+	
+	/**
+	 * Equality check for Matrix. Note: that it can be expensive as it compares 
+	 * every element in both matrices for equality
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Matrix){
+			Matrix m = (Matrix) obj;
+			if (m.rows() != rows() || m.cols() != cols()){
+				return false;
+			}
+			for (int row = 0; row < rows(); row++){
+				for (int col = 0; col < cols(); col++){
+					if (Math.abs(m.get(row, col) - get(row, col)) > EQUALITY_EPSILON){
+						return false;
+					}
+				}
+			}
+			return true;
+		} else{
+			return super.equals(obj);
+		}
 	}
 }
---
+++ src/main/Vector.java
@@ -0,0 +1,127 @@
+package main;
+
+public class Vector extends Matrix {
+	
+	private int _n;
+	
+	/**
+	 * This is a private constructor so that people can't create a vector that 
+	 * has 2d, i.e. put it in a bad state
+	 */
+	private Vector(int rows, int cols){
+		super(rows, cols);
+		_n = Math.max(rows, cols);
+	}
+	
+	public static Vector rowVector(int cols){
+		return new Vector(1, cols);
+	}
+	
+	public static Vector rowVector(double[] d, int cols){
+		Vector v = new Vector(1, cols);
+		for (int i = 0; i < cols; i++){
+			v.set(i, d[i]);
+		}
+		return v;
+	}
+	
+	public static Vector rowVector(Matrix m, int row){
+		Vector v = new Vector(1, m.cols());
+		for (int i = 0; i < m.cols(); i++){
+			v.set(i, m.get(row, i));
+		}
+		return v;
+	}
+	
+	public static Vector columnVector(Matrix m, int col){
+		Vector v = new Vector(m.rows(), 1);
+		for (int i = 0; i < m.rows(); i++){
+			v.set(i, m.get(i, col));
+		}
+		return v;
+	}
+	
+	public static Vector columnVector(int rows){
+		return new Vector(rows, 1);
+	}
+	
+	public static Vector columnVector(double[] d, int rows){
+		Vector v = new Vector(rows, 1);
+		for (int i = 0; i < rows; i++){
+			v.set(i, d[i]);
+		}
+		return v;
+	}
+	
+	/**
+	 * Convenience method for setting values in the vector to avoid indexing issues
+	 * @param i
+	 * @param value
+	 */
+	private void set(int i, double value){
+		if (isRowVector()){
+			_m[0][i] = value;
+		} else{
+			_m[i][0] = value;
+		}
+	}
+	
+	/**
+	 * Returns the length of the vector 
+	 * @return
+	 */
+	public int length(){
+		return _n;
+	}
+	
+	/**
+	 * Returns whether the vector is a row vector. Note if vector has length 1, 
+	 * it is neither a row or column vector
+	 * @return
+	 */
+	public boolean isRowVector(){
+		return (rows() == 1 && cols() > 1);
+	}
+	
+	/**
+	 * Returns whether the vector is a column vector. Note if vector has length 1, 
+	 * it is neither a row or column vector
+	 * @return
+	 */
+	public boolean isColumnVector(){
+		return !isRowVector();
+	}
+	
+	/**
+	 * Convenience method for getting the elements of a vector without 
+	 * knowing if it is a row or column vector
+	 * @param i
+	 * @return
+	 */
+	public double get(int i){
+		if (isRowVector()){
+			return get(0, i);
+		} else{
+			return get(i, 0);
+		}
+	}
+	
+	/**
+	 * Performs a vector dot product. The vectors must be the same length
+	 * @throws IllegalArgumentException if the vectors differ in length
+	 * @param v
+	 * @return
+	 */
+	public double dot(Vector v){
+		if (v.length() != length()){
+			throw new IllegalArgumentException("Dot product must be between vectors" +
+												" of the same length. this.length()== " + 
+												length() + ", v.length() == " + v.length());
+		}
+		double sum = 0;
+		for (int i = 0; i < length(); i++){
+			sum += v.get(i) * get(i);
+		}
+		return sum;	
+	}
+}
